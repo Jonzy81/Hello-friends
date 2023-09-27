5c1840340ffb1f019d532b2d21271bc57654f9fa
jonnytouma <jonnytouma@gmail.com>
2023-09-27 14:19:29 +0200

    IF statement

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace ConsoleApp1
+﻿using System.Threading.Channels;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hej sean, Hej Jing, Hej Niklas");
+            Console.WriteLine("Are you a friend of mine? y or n");
+            string friend=Console.ReadLine();
+            if(friend == "y")
+            {
+                Console.WriteLine("tönt");
+            }
         }
+        
     }
 }
